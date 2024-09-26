272d29b7bd1ef9593ca934639b50c958aabbacd0
GalaxyJumper <oliverdvis@outlook.com>
2024-09-25 17:46:45 -0700

    Update todo list

--- Main.java
+++ Main.java
@@ -3,8 +3,22 @@
  * 
  *  Create face list + sorting [1]
  *  Add lighting [1]
- *  Fix weird perspective distortion
+ *  Fix weird perspective distortion [1]
  *  Setup input nonsense [1]
+ *  Color the cuboids [0]
+ *  Support animation [0]
+ *  Write RotateAbsolute [0]
+ *  Fix weird overlap stuff [0]
+ *  Comment your code lmfao [0]
+ * 
+ *  Long term (to ponder for now)
+ *  Sorted in order of possibility:
+ *  Actual lighting
+ *  Texture mapping
+ *  Occlusion culling
+ *  BSP
+ *  Ray tracing (??)
+ *  
  */
 
 import java.awt.AWTException;
